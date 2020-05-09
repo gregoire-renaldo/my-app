80de8eb4e1836d1fddc965587c33e74cd4e19b4c
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-05-09 14:47:32 +0200

    model decks

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_193707) do
+ActiveRecord::Schema.define(version: 2020_05_09_124615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_decks_on_user_id"
+  end
 
   create_table "places", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_04_24_193707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "decks", "users"
   add_foreign_key "places", "users"
 end

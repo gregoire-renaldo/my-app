53165ba49bb807b70254c721bf6d161ac27e2a6a
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-05-11 12:53:47 +0200

    login & logout ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [ :index, :show, :edit, :update ]
   resources :places

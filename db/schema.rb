f52a4c3cef0904e271f0f0f364041d21c39aacdf
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-08 21:41:14 +0200

    seeds ok, missing multiple users for gamesession

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_160352) do
+ActiveRecord::Schema.define(version: 2020_06_08_190225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_05_14_160352) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

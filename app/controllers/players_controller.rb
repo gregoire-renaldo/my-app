8ba0692796013d171aa760dc88eae8793a6e999e
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-15 13:39:53 +0200

    joining the game working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,9 +6,25 @@ class PlayersController < ApplicationController
   end
 
   def new
+    @gamesession = GameSession.find(params[:game_session_id])
+    @player = Player.new
+    # if current_user.role == "designer"
+    # @request = Request.new
+    # else
+    #   redirect_to projects_path
+    # end
   end
 
   def create
+    @player = Player.new(player_params)
+    @gamesession = GameSession.find(params[:game_session_id])
+    @player.game_session = @gamesession
+    @player.user_id = current_user.id
+    if @player.save
+      redirect_to game_session_path(@gamesession)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +34,11 @@ class PlayersController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:gamesession_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :game_sessions
-  resources :players
+  resources :game_sessions do
+    resources :players
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

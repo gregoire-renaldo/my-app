cf55638364dd15d327a8e053806c7646ad642981
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-16 15:05:19 +0200

    join only one time a gamesession

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
   belongs_to :game_session
   belongs_to :user
+
+  validates_uniqueness_of :user_id, :scope => [:game_session_id], :message => "you have already joined this game session"
 end
